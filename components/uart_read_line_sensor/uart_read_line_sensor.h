a15e720d2cfda64be355dae3e72438352f50e93f
Wez Furlong <wez@wezfurlong.org>
2025-08-29 02:25:13 -0700

    refine buffer manipulation

--- components/uart_read_line_sensor/uart_read_line_sensor.h
+++ components/uart_read_line_sensor/uart_read_line_sensor.h
@@ -14,37 +14,25 @@ namespace esphome {
                 void setup() override;
                 
                 void loop() override {
-                    const int max_line_length = 80;
-                    static char buffer[max_line_length];
                     while (available()) {
-                        if (readline(read(), buffer, max_line_length) > 0) {
-                            publish_state(buffer);
-                        }
+                        int byte = read();
+                        switch (byte) {
+                            case '\n':
+                                break;
+                            case '\r':
+                                publish_state(m_buffer);
+                                m_buffer.clear();
+                                break;
+                            default:
+                                if (byte > 0) {
+                                    m_buffer.push_back(byte);
+                                }
+                        }                                
                     }
                 }
                 
             protected:
-                int readline(int readch, char *buffer, int len) {
-                    static int pos = 0;
-                    int rpos;
-                    if (readch > 0) {
-                        switch (readch) {
-                            case '\n': 
-                                break;
-                            case '\r':
-                                rpos = pos;
-                                pos = 0;
-                                return rpos;
-                            default:
-                                if (pos < len - 1) {
-                                    buffer[pos++] = readch;
-                                    buffer[pos] = 0;
-                                }
-                        }
-                    }
-                    return -1;
-                }
+                std::string m_buffer;
         };
-        
     }  // namespace uart_read_line_sensor
 }  // namespace esphome
